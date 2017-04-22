245fc394deb008a31bd5caa2ab11981d7c20eb79
Justin DiMichele <Justin DiMichele>
2017-04-22 17:05:33 -0400

    added asteroid rotation'

--- Blue/Assets/AsteroidController.cs
+++ Blue/Assets/AsteroidController.cs
@@ -6,19 +6,23 @@ public class AsteroidController : MonoBehaviour {
 
 
 	public int health = 100;
-
-
-
-
+	public float minSpeed = 1f;
+	public float maxSpeed = 1.5f;
 	// Use this for initialization
 	void Start () {
 		
+		float xSpin = Random.Range(minSpeed, maxSpeed);
+		float ySpin = Random.Range(minSpeed, maxSpeed);
+		float zSpin = Random.Range(minSpeed, maxSpeed);
+
+		GetComponent < Rigidbody > ().AddTorque (new Vector3 (xSpin, ySpin, zSpin));
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (health <= 0){
+			if (health <= 0){
 			Destroy (transform.gameObject);
+
 		}
 	}
 }
