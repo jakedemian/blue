f804f7329b2c46cb74dd2c4562577dd347c2198d
Justin DiMichele <Justin DiMichele>
2017-04-25 16:35:49 -0400

    asteroid fragment physics updated

--- Blue/Assets/Scripts/AsteroidController.cs
+++ Blue/Assets/Scripts/AsteroidController.cs
@@ -8,10 +8,30 @@ public class AsteroidController : MonoBehaviour {
 	public int health = 100;
 	public float minSpeed = 1f;
 	public float maxSpeed = 1.5f;
+	private float minFragmentSpinSpeed = 30f;
+	private float maxFragmentSpinSpeed = 35f;
+	private float minFragmentSpeed = 3f;
+	private float maxFragmentSpeed = 6f;
 
-	// Use this for initialization
+	public GameObject AsteroidFragment;
+
+
+	void spawnAsteroidParticle(){
+		GameObject go = Instantiate (AsteroidFragment, transform.position, Quaternion.identity);
+
+		float speed = Random.Range (minFragmentSpeed, maxFragmentSpeed);
+		Vector3 randomUnitOnSphere = Random.onUnitSphere;
+
+		go.transform.position = new Vector3 (go.transform.position.x + randomUnitOnSphere.x, go.transform.position.y + randomUnitOnSphere.y, go.transform.position.z);
+		go.GetComponent<Rigidbody>().velocity = randomUnitOnSphere * speed;
+		go.GetComponent<Rigidbody>().angularVelocity = randomUnitOnSphere * speed;
+
+
+	}
+
+
+
 	void Start() {
-		
 		float xSpin = Random.Range(minSpeed, maxSpeed);
 		float ySpin = Random.Range(minSpeed, maxSpeed);
 		float zSpin = Random.Range(minSpeed, maxSpeed);
@@ -21,7 +41,14 @@ public class AsteroidController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-			if (health <= 0){
+		if (health <= 0){
+			
+			int numberOfParticles = Random.Range (5, 10);
+
+			for (int i = 0; i < numberOfParticles; i++){
+				spawnAsteroidParticle();
+			}
+
 			Destroy (transform.gameObject);
 		}
 	}
